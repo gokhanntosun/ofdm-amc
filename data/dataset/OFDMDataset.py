18819e38e4f40d6e30a7931313660e851808aca0
gtosun <gokhantosun@live.com>
2023-04-05 22:47:32 +0300

    set correct data types

--- data/dataset/OFDMDataset.py
+++ data/dataset/OFDMDataset.py
@@ -12,16 +12,14 @@ class OFDMDataset(Dataset):
         return self.xs.shape[-1]
 
     def __getitem__(self, index):
-        return (self.xs[:, :, index], self.ys[index])
+        return (self.xs[:, :, index], self.ys[index, :])
 
     def __read_data(self):
         # If change BASE to one upper directory, it will mix all data points. Currently it seperates by SNR values
-        BASE = '/Users/gtosun/Documents/vsc_workspace/ofdm-amc/data/data_lib'
+        BASE = '/Users/gtosun/Documents/vsc_workspace/ofdm-amc/data/data_lib/20db'
 
         NUMBER_OF_ITEMS = 1000
         SYMBOL_PER_ITEM = 2048
-
-        # could not broadcast input array from shape (2,2048,1000) into shape (1000,2,2048)
 
         xpaths, ypaths = [], []
         cnt = 0
@@ -31,19 +29,18 @@ class OFDMDataset(Dataset):
                 if file.startswith('y'): ypaths.append(os.path.join(root, file))
 
         cnt = len(xpaths)
-        # xs = np.zeros((NUMBER_OF_ITEMS * cnt, 2, SYMBOL_PER_ITEM))
         xs = np.zeros((2, SYMBOL_PER_ITEM, NUMBER_OF_ITEMS * cnt))
-        ys = [""] * (cnt * NUMBER_OF_ITEMS)
+        ys = np.zeros((cnt * NUMBER_OF_ITEMS, 3), dtype=np.float32)
 
         for xp, yp in zip(xpaths, ypaths):            
             assert os.path.exists(xp), f'Invalid path: {xp}'
             assert os.path.exists(yp), f'Invalid path: {yp}'
 
-            x = torch.from_numpy(np.load(xp)).double()
-            y = np.load(yp)
+            x = torch.from_numpy(np.load(xp)).to(torch.float32)
+            y = torch.from_numpy(np.load(yp)).to(torch.float32)
 
-            xs[:, :, (cnt - 1) * NUMBER_OF_ITEMS: cnt * NUMBER_OF_ITEMS] = x.double()
-            ys[(cnt - 1) * NUMBER_OF_ITEMS: cnt * NUMBER_OF_ITEMS] = y
+            xs[:, :, (cnt - 1) * NUMBER_OF_ITEMS: cnt * NUMBER_OF_ITEMS] = x
+            ys[(cnt - 1) * NUMBER_OF_ITEMS: cnt * NUMBER_OF_ITEMS, :] = y
             cnt -= 1
             assert cnt >= 0
         assert cnt == 0
@@ -52,7 +49,7 @@ class OFDMDataset(Dataset):
 def get_dataloaders(dataset: OFDMDataset, batch_size: int, shuffle: bool):
     train_size = int(len(dataset) * 0.85)
     test_size = len(dataset) - train_size
-    train_set, test_set = random_split(dataset=dataset, lengths=[train_size ,test_size], generator=torch.Generator().manual_seed(0))
+    train_set, test_set = random_split(dataset=dataset, lengths=[train_size ,test_size])
 
     trainloader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=shuffle)
     testloader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=shuffle)
