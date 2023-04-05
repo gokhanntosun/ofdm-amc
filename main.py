9944f245f6109aa85b073e074a23d19fa8367dbd
gtosun <gokhantosun@live.com>
2023-04-05 22:48:41 +0300

    ready for training process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import torch
+import numpy as np
 from model.AMCModel import AMCModel
 from data.dataset.OFDMDataset import OFDMDataset, get_dataloaders
 from torch.optim import Adam
@@ -7,49 +8,52 @@ import torch.nn as nn
 
 def main():
 
-    LR = 0.0005
+    LR = 0.0003
     BATCH_SIZE = 1
     SHUFFLE = True
-    EPOCHS = 5
+    EPOCHS = 1
 
     dataset = OFDMDataset()
     trainloader, testloader = get_dataloaders(dataset=dataset, batch_size=BATCH_SIZE, shuffle=SHUFFLE)
 
-    model = AMCModel().double()
-    criterion = nn.BCELoss()
+    model = AMCModel()
+    model = model.train()
+    criterion = nn.CrossEntropyLoss()
     optimizer = Adam(params=model.parameters(), lr=LR)
 
+
+    losses, predictions = [], []
     n_total_steps = len(trainloader)
     for epoch in range(EPOCHS):
+
+        model = model.train()
         for i, (x, y) in enumerate(trainloader):
-            scores = model.forward(x)
-            loss = criterion(scores,torch.squeeze(y))
             optimizer.zero_grad()
+            scores = model(x)
+            loss = criterion(scores, torch.squeeze(y))
             loss.backward()
             optimizer.step()
 
+            predictions.append(torch.argmax(scores).item())
+            losses.append(loss.item())
+
             if (i+1) % 100 == 0:
-                print (f'Epoch [{epoch+1}/{EPOCHS}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
-    
-    param_cnt = 0
-    for param in model.parameters():
-        param_cnt += torch.prod(torch.tensor(param.shape))
+                print (f'Epoch [{epoch+1}/{EPOCHS}], Step [{i+1}/{n_total_steps}], Loss: {np.mean(losses):.4f}')
+                losses.clear()
 
-    print(f'Number of parameters: {param_cnt:,}')
-    torch.save(model.state_dict(), "/Users/gtosun/Documents/vsc_workspace/ofdm-amc/min_model.pth")
+        model = model.eval()
+        with torch.no_grad():
+            n_correct = 0
+            n_samples = 0
+            for x, y in testloader: 
+                scores = model.forward(x)
+                predicted = torch.argmax(scores).item()
+                labels = torch.argmax(torch.squeeze(y)).item()
+                n_samples += y.size(0)
+                n_correct += predicted == labels
 
-    with torch.no_grad():
-        n_correct = 0
-        n_samples = 0
-        for x, y in testloader:
-            scores = model.forward(x.double())
-            _, predicted = torch.max(scores.data, 1)
-            _, labels = torch.max(torch.squeeze(y), 1)
-            n_samples += y.size(0)
-            n_correct += (predicted == labels).sum().item()
-
-        acc = 100.0 * n_correct / n_samples
-        print(f'Accuracy of the network on the 10000 test set: {acc} %')
+            acc = 100.0 * n_correct / n_samples
+            print(f'Accuracy of the network on the test set: {acc} %')
 
 if __name__ == "__main__":
     sys.exit(main())
--- model/AMCModel.py
+++ model/AMCModel.py
@@ -15,10 +15,11 @@ class AMCModel(nn.Module):
         self.__cnn512 = AMCModel.SubNetwork(fft_size=512)
         self.__cnn1024 = AMCModel.SubNetwork(fft_size=1024)
 
-        self.__fc1 = nn.Linear(in_features=12288, out_features=128)
+        self.__fc1 = nn.LazyLinear(out_features=128)
         self.__fc2 = nn.Linear(in_features=128, out_features=64)
         self.__fc3 = nn.Linear(in_features=64, out_features=3)
-        self.__smax = nn.Softmax(dim=0)
+
+        self.__tanh = nn.Tanh()
 
     def forward(self, x):
         f256 = self.__cnn256.forward(x)
@@ -26,34 +27,24 @@ class AMCModel(nn.Module):
         f1024 = self.__cnn1024.forward(x)
         w = torch.cat([f256, f512, f1024])
 
-        w = self.__fc1(w)
-        w = self.__fc2(w)
-        w = self.__fc3(w)
-
-        return self.__smax(w)
+        w = self.__tanh(self.__fc1(w))
+        w = self.__tanh(self.__fc2(w))
+        return self.__fc3(w)
     
     class BaseModel(nn.Module):
         def __init__(self) -> None:
             super().__init__()
-
-            self.conv1 = nn.Conv2d(1, 256, (2, 1), padding=(1, 0))
-            self.conv2 = nn.Conv2d(256, 128, (1, 3), padding=(0, 1))
-            self.conv3 = nn.Conv2d(128, 64, (1, 3), padding=(0, 1))
-            self.conv4 = nn.Conv2d(64, 32, (1, 5), padding=(0, 2))
-            self.conv5 = nn.Conv2d(32, 16, (1, 7), padding=(0, 3))
-
-            self.max_pool= nn.MaxPool2d((2, 1), padding=(1, 0))
-            self.relu = nn.ReLU()
+            self.conv1 = nn.Conv2d(1, 32, (2, 4), padding=(1, 2))
+            self.conv2 = nn.Conv2d(32, 4, (2, 2), padding=(1, 1))
+            self.avg_pool= nn.AvgPool2d((2, 2), padding=(1, 1))
+            self.relu = nn.LeakyReLU(negative_slope=0.05)
             
 
         def forward(self, x):
             x = torch.unsqueeze(x, 0)
-            x = self.max_pool( self.relu( self.conv1(x) ) )
-            x = self.max_pool( self.relu( self.conv2(x) ) )
-            x = self.max_pool( self.relu( self.conv3(x) ) )
-            x = self.max_pool( self.relu( self.conv4(x) ) )
-            x, _ = torch.max(x, dim=0)
-            x = self.relu(x)
+            x = self.avg_pool( self.relu( self.conv1(x) ) )
+            x = self.avg_pool( self.relu( self.conv2(x) ) )
+
             return torch.flatten(x)
 
     class SubNetwork(BaseModel, nn.Module):
@@ -63,4 +54,4 @@ class AMCModel(nn.Module):
 
         def forward(self, x):
             x = self.__filter.filter(x)
-            return super().forward(torch.from_numpy(x))
+            return super().forward(x)
