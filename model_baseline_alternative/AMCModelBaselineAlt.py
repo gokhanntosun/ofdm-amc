de654fb4c43e929e6da41dbaa690b25b7647b8d6
gtosun <gokhantosun@live.com>
2023-06-10 20:28:47 +0300

    initial commit for alternative baseline model

---
+++ model_baseline_alternative/AMCModelBaselineAlt.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+from model.FilterBank import FilterBank
+        
+class AMCModelBaselineAlt(nn.Module):
+    def __init__(self, filter_size_list: list=[256, 512, 1024]) -> None:
+        super().__init__()
+
+        self.f1 = FilterBank.FFTFilter(n=256)
+        self.f2 = FilterBank.FFTFilter(n=512)
+        self.f3 = FilterBank.FFTFilter(n=1024)
+
+        self.conv1 = nn.Conv2d(1, 32, (7, 1), padding=(3, 0))
+        self.conv2 = nn.Conv2d(32, 16, (3, 3), padding=(1, 1))
+        self.conv3 = nn.Conv2d(16, 4, (2, 2), padding=(1, 1))
+        self.conv4 = nn.Conv2d(4, 1, (2, 1), padding=(1, 0))
+
+        self.fc1 = nn.LazyLinear(out_features=128)
+        self.fc2 = nn.Linear(in_features=128, out_features=64)
+        self.fc3 = nn.Linear(in_features=64, out_features=3)
+
+        self.mpool1 = nn.MaxPool2d((2, 1), stride=(1, 1), padding=(0, 0))
+        self.mpool2 = nn.MaxPool2d((2, 2), padding=(0, 0))
+
+        self.relu = nn.LeakyReLU(negative_slope=0.05)
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+
+        x1 = self.f1.filter(x)
+        x2 = self.f2.filter(x)
+        x3 = self.f3.filter(x)
+        x = torch.row_stack((x1, x2, x3))
+
+        x = torch.unsqueeze(torch.unsqueeze(x, 0), 0)
+        x = self.mpool1( self.relu( self.conv1(x) ) )
+        x = self.mpool2( self.relu( self.conv2(x) ) )
+        x = self.mpool2( self.relu( self.conv3(x) ) )
+        x = self.mpool2( self.relu( self.conv4(x) ) )
+
+        x = torch.flatten(x)
+
+        x = self.tanh( self.fc1(x) )
+        x = self.tanh( self.fc2(x) )
+        return torch.squeeze(self.fc3(x))
---
+++ model_baseline_alternative/__init__.py
@@ -0,0 +1,3 @@
+from .AMCModelBaselineAlt import (
+    AMCModelBaselineAlt,
+    )
